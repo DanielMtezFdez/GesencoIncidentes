adcf348f1b29037e0ecab7d0e89d5e4fe1c85c84
DanielMtezFdez <dani.martinez.3.fernandez@gmail.com>
2019-10-02 02:59:53 +0200

    Eliminación de parámetros innecesarios

--- src/database_firebird/Main.java
+++ src/database_firebird/Main.java
@@ -6,10 +6,6 @@ import java.util.ArrayList;
 
 public class Main {
 
-    private static String url = "jdbc:firebirdsql:localhost/3050:D:\\DISTR K\\HORIZONT\\Database\\HORIZONT.FDB";
-    private static String user = "SYSDBA";
-    private static String pass = "masterkey";
-
     public static void main(String[] args) {
         ArrayList<Comunidad> comunidades = ComunidadDAO.getComunidades();
         for (Comunidad c : comunidades){
