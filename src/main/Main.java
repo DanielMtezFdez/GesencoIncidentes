caec622277760a43f1944f7a229d0fd232b584ea
DanielMtezFdez <dani.martinez.3.fernandez@gmail.com>
2019-10-12 00:58:48 +0200

    Modificaciones en UI

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Gesenco Incidentes");
         primaryStage.setScene(new Scene(root, 1315, 807));
         primaryStage.setResizable(false);
         primaryStage.show();
