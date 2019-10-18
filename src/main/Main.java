fc73c4af1a1fb4fac224f1c6ce631fb7ce095269
DanielMtezFdez <dani.martinez.3.fernandez@gmail.com>
2019-10-18 19:05:03 +0200

    Implementacion icono empresa y buildeo .jar

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +17,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
         primaryStage.setTitle("Gesenco Incidentes");
+
+        Image icon = new Image("img/logo_gesenco.jpg");
+        primaryStage.getIcons().add(icon);
+
         primaryStage.setScene(new Scene(root, 1315, 807));
         primaryStage.setResizable(false);
         primaryStage.show();
